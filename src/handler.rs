d656df951f17aa3814575456ef41faef5af94a9a
kotokz <kotokz@live.com>
2017-08-06 09:39:29 +0800

    fix file format

--- src/handler.rs
+++ src/handler.rs
@@ -1,1 +1,18 @@
-use rocket_contrib::Json;use padding;use params;#[get("/leftpad?<request>")]fn leftpad(    request: params::PaddingRequestUrl,) -> Result<Json<padding::PaddingResponse>, &'static str> {    Ok(Json(padding::left(&params::read_params(&request)?)))}#[get("/rightpad?<request>")]fn leftpad_right(    request: params::PaddingRequestUrl,) -> Result<Json<padding::PaddingResponse>, &'static str> {    Ok(Json(padding::right(&params::read_params(&request)?)))}
+use rocket_contrib::Json;
+use padding;
+use params;
+
+#[get("/leftpad?<request>")]
+fn leftpad(
+    request: params::PaddingRequestUrl,
+) -> Result<Json<padding::PaddingResponse>, &'static str> {
+
+    Ok(Json(padding::left(&params::read_params(&request)?)))
+}
+
+#[get("/rightpad?<request>")]
+fn leftpad_right(
+    request: params::PaddingRequestUrl,
+) -> Result<Json<padding::PaddingResponse>, &'static str> {
+    Ok(Json(padding::right(&params::read_params(&request)?)))
+}
